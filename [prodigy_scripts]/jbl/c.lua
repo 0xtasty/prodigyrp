c15ede8cf79ac30cb52dc5fe5f4f44b8e1f6f2cb
GatonBr12 <dimasjuniorbr728@gmail.com>
2022-07-07 14:04:04 -0300

    organização

---
+++ [prodigy_scripts]/jbl/c.lua
@@ -0,0 +1,122 @@
+radioSound = {}
+addEventHandler("onClientResourceStart", resourceRoot,
+	function()
+		txd = engineLoadTXD("assets/bl.txd", 2226 )
+		engineImportTXD(txd, 2226)
+		dff = engineLoadDFF("assets/jbl.dff", 2226 )
+		engineReplaceModel(dff, 2226)
+		bindKey("r", "down", clientToggleRadio)
+	end
+)
+addEventHandler("onClientSoundStream", root,
+	function(success, length, streamName)
+		if streamName then
+			local veh = getPedOccupiedVehicle(getLocalPlayer())
+			if veh then
+				if radioSound[veh] == nil then return end
+				if radioSound[veh].soundElement == source then
+					outputChatBox("#696969R�dio: #22AA22 " .. streamName, 0, 0, 0, true)
+				end
+			end
+		end
+	end
+)
+function pegar_jbl (button, state, absoluteX, absoluteY, worldX, worldY, worldZ, clickedElement)
+    triggerServerEvent("one:pegarjbl", getLocalPlayer(), getLocalPlayer(), clickedElement)
+end
+addEventHandler ( "onClientClick", root, pegar_jbl )
+addEventHandler("onClientSoundChangedMeta", root,
+	function(streamTitle)
+		if streamTitle then
+			local veh = getPedOccupiedVehicle(getLocalPlayer())
+			if veh then
+				if radioSound[veh] == nil then return end
+				if radioSound[veh].soundElement == source then
+					outputChatBox("#696969M�sica: #AA2222 " .. streamTitle, 0, 0, 0, true)s
+				end
+			end
+		end
+	end
+)
+addEvent("onServerToggleRadio", true)
+addEventHandler("onServerToggleRadio", getLocalPlayer(), 
+	function(toggle, url, veh, volume)
+		if not isElement(veh) then
+			if radioSound[veh] ~= nil then
+				stopSound(radioSound[veh].soundElement)
+				radioSound[veh].soundElement = nil
+			end
+			return
+		end
+		
+		if toggle == true then
+			local x, y, z = getElementPosition(veh)
+            print(radioSound[veh])
+			if radioSound[veh] ~= nil then
+				stopSound(radioSound[veh].soundElement)
+                
+				local sound = playSound3D(url, x, y, z)
+				if volume ~= nil then
+					setSoundVolume(sound, volume)
+				end
+				setSoundMinDistance(sound, 5.0)
+				setSoundMaxDistance(sound,10.0)
+				attachElements(sound, veh)
+				
+				radioSound[veh] = { }
+				radioSound[veh].soundElement = sound
+			else
+				local sound = playSound3D(url, x, y, z)
+                print('entrei??')
+				if volume ~= nil then
+					setSoundVolume(sound, volume)
+				end
+				setSoundMinDistance(sound, 5.0)
+				setSoundMaxDistance(sound, 10.0)
+				attachElements(sound, veh)
+				
+				radioSound[veh] = { }
+				radioSound[veh].soundElement = sound
+			end
+		else
+			if radioSound[veh] ~= nil then
+				stopSound(radioSound[veh].soundElement)
+				radioSound[veh].soundElement = nil
+			end
+		end
+	end
+)
+addEvent("onServerRadioURLChange", true)
+addEventHandler("onServerRadioURLChange", getLocalPlayer(), 
+	function(newurl, veh, volume)
+		if radioSound[veh] ~= nil then
+			stopSound(radioSound[veh].soundElement)
+		
+			local x, y, z = getElementPosition(veh)
+            print(x,y,z)
+			local sound = playSound3D(newurl, x, y, z)
+			if volume ~= nil then
+				setSoundVolume(sound, volume)
+			end
+			setSoundMinDistance(sound, 5)
+			setSoundMaxDistance(sound, 10.0)
+			attachElements(sound, veh)
+		
+			radioSound[veh] = { }
+			radioSound[veh].soundElement = sound
+		end
+	end
+)
+addEvent("onServerVolumeChangeAccept", true)
+addEventHandler("onServerVolumeChangeAccept", getLocalPlayer(), 
+	function(veh, newVolume)
+		if veh then
+			if radioSound[veh] ~= nil then
+				setSoundVolume(radioSound[veh].soundElement, newVolume)
+			end
+		end
+	end
+)
+function clientToggleRadio()
+	triggerServerEvent("onPlayerToggleRadio", getLocalPlayer())
+end
---
+++ [prodigy_scripts]/prodigy_spawn/script.lua
@@ -0,0 +1,58 @@
+﻿-- Configurações para quando um jogador entrar
+	-- XYZ coodenadas
+	local joinX = 1480.705
+	local joinY = -1768.473    
+	local joinZ = 18.796
+	-- Armas e tiros
+	--local joinWeapon = 100
+	--local joinAmmo = 90
+	-- Menssagem para o jogador
+	--local joinMessage = "~>[S]ilver!"
+	-- Pele do jogador
+	--local joinSkin = 19
+
+-- Configurações para quando um jogador morrer
+	-- XYZ coördinates
+	local spawnX = 2037.885
+	local spawnY = -1412.131
+	local spawnZ = 17.164
+	-- Armas e tiros
+	--local spawnWeapon = 31
+	--local spawnAmmo = 100
+	-- Pele do jogador
+	--local spawnSkin = 27
+
+-- Definições para as quantidades de dinheiro
+	-- Dinheiro por matar um jogador
+	--local killerMoney = 500
+	-- O dinheiro retirado do jogador quando morrem
+	--local deadPlayerMoney = 50
+	-- Dinheiro ganho quando entra no server
+	local joinMoney = 9000
+
+-- Functions
+-- This function spawns players when they join
+function spawnOnJoin()
+	spawnPlayer(source, joinX, joinY, joinZ, 0 , joinSkin)
+	fadeCamera(source, true)
+	setCameraTarget(source, source)
+	outputChatBox(joinMessage, source)
+	giveWeapon(source, joinWeapon, joinAmmo)
+	givePlayerMoney(source, joinMoney)
+end
+
+-- This function spawns players after they died
+function spawnOnDead(ammo, killer, weapon, bodypart)
+	outputChatBox(getPlayerName(source).." died.")
+	takePlayerMoney(source, deadPlayerMoney)
+	if (killer) and (killer ~= source) then
+		givePlayerMoney(killer, killerMoney)
+	end
+	setTimer(spawnPlayer, 3000, 1, source, spawnX, spawnY, spawnZ, 0, spawnSkin)
+	setCameraTarget(source, source)
+	setTimer(giveWeapon, 3000, 1, source, spawnWeapon, spawnAmmo)
+end
+
+-- Event handlers
+addEventHandler("onPlayerJoin", getRootElement(), spawnOnJoin)
+addEventHandler("onPlayerWasted", getRootElement(), spawnOnDead)
